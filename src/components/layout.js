efee0a9374f7425f32a33f827eb3f8e94a9c4742
Carlos Edinson Roso Espinosa <ce.roso398@gmail.com>
2024-09-29 00:25:07 +0200

    remove banner from home

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,14 +5,14 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import Header from "./header"
-import Footer from "./footer"
-import "./layout.scss"
-import CttiBanner from "./ctti-banner"
+import Header from './header';
+import Footer from './footer';
+import './layout.scss';
+// import CttiBanner from './ctti-banner';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -23,26 +23,26 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
-      <div className="layout__ctti">
+      {/* <div className="layout__ctti">
         <CttiBanner />
-      </div>
+      </div> */}
       <div className="layout__header">
         <Header siteTitle={data.site.siteMetadata.title} />
       </div>
       <main className="layout__main">{children}</main>
       <div className="container">
-        <Footer/>
+        <Footer />
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
